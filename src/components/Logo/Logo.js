6827dff09df6ace2d3048b7d5f4e7f3c432fc4d0
Muhammad Lawan <mlawan252@gmail.com>
2025-01-16 14:44:44 +0100

    changed the logo design

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,11 @@
 import styles from "./Logo.module.css"
 
 function Logo(){
-    return <img src="knLogo1.png" alt="logo" className={styles.logo} />
+    return (
+        <div className={styles.logoContainer}>
+            <img src="kn-logo.png" alt="logo" className={styles.logo} />
+            <p>Ministry of Power and Renewable Energy</p>
+        </div>
+    ) 
 }
 export default Logo;
