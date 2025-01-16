aded52aaa59207c2f35dfd24df2b639487ee8f50
Muhammad Lawan <mlawan252@gmail.com>
2025-01-16 13:59:42 +0100

    change logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import styles from "./Logo.module.css"
 
 function Logo(){
-    return <img src="kano-logo.png" alt="logo" className={styles.logo} />
+    return <img src="knLogo1.png" alt="logo" className={styles.logo} />
 }
 export default Logo;
